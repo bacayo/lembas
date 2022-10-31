9884d66fbdfb7b8dcfddb2e058bbf5a5b09684e7
bacayo <batuhanyolcuu@gmail.com>
2022-10-31 10:12:31 +0300

    resize loading animation

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import Lottie from 'lottie-react-native';
+import { View, StyleSheet } from 'react-native';
 
 const Loading = () => {
   return (
-    <Lottie
-      source={require('../../../assets/448-ripple-loading-animation.json')}
-      autoPlay={true}
-      loop={true}
-    />
+    <View style={styles.container}>
+      <Lottie
+        style={styles.lottie}
+        source={require('../../../assets/448-ripple-loading-animation.json')}
+        autoPlay={true}
+        loop={true}
+      />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  lottie: {
+    alignSelf: 'center',
+    height: 200,
+    width: 200,
+  },
+});
 
 export default Loading;
